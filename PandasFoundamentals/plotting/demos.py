68ce2e3bc5b72e8ef6f200178f6e223800d0a422
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2022-01-27 15:37:48 +0100

    More plots

--- PandasFoundamentals/plotting/demos.py
+++ PandasFoundamentals/plotting/demos.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True,
+                 'axes.titlepad': 20})
 from PandasFoundamentals.helperClasses.helperClass import HelperClass
 
 helper = HelperClass()
@@ -9,3 +13,78 @@ df = pd.read_pickle(os.path.join(currentDirectory, 'data', 'data_frame.pickle'))
 # Simplest default plot
 acquisition_years = df.groupby('acquisitionYear').size()
 acquisition_years.plot()
+
+fig = plt.figure()
+sublot = fig.add_subplot(1,1,1)
+acquisition_years.plot(ax = sublot)
+fig.show()
+
+# Add axis labels
+fig = plt.figure()
+subplot = fig.add_subplot(1, 1, 1)
+acquisition_years.plot(ax=subplot)
+subplot.set_xlabel("Acquisition Year")
+subplot.set_ylabel("Artworks Acquired")
+fig.show()
+
+
+# Increase ticks granularity
+fig = plt.figure()
+subplot = fig.add_subplot(1, 1, 1)
+acquisition_years.plot(ax=subplot)
+subplot.set_xlabel("Acquisition Year")
+subplot.set_ylabel("Artworks Acquired")
+subplot.locator_params(nbins=40, axis='x')
+fig.show()
+
+# Rotate X ticks
+fig = plt.figure()
+subplot = fig.add_subplot(1, 1, 1)
+acquisition_years.plot(ax=subplot, rot=45)
+subplot.set_xlabel("Acquisition Year")
+subplot.set_ylabel("Artworks Acquired")
+subplot.locator_params(nbins=40, axis='x')
+fig.show()
+
+# Add log scale
+fig = plt.figure()
+subplot = fig.add_subplot(1, 1, 1)
+acquisition_years.plot(ax=subplot, rot=45, logy=True)
+subplot.set_xlabel("Acquisition Year")
+subplot.set_ylabel("Artworks Acquired")
+subplot.locator_params(nbins=40, axis='x')
+fig.show()
+
+# Add grid
+fig = plt.figure()
+subplot = fig.add_subplot(1, 1, 1)
+acquisition_years.plot(ax=subplot, rot=45, logy=True, grid=True)
+subplot.set_xlabel("Acquisition Year")
+subplot.set_ylabel("Artworks Acquired")
+subplot.locator_params(nbins=40, axis='x')
+fig.show()
+
+# Set fonts
+title_font = {'family': 'source sans pro',
+        'color':  'darkblue',
+        'weight': 'normal',
+        'size': 20,
+        }
+labels_font = {'family': 'consolas',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+fig = plt.figure()
+subplot = fig.add_subplot(1, 1, 1)
+acquisition_years.plot(ax=subplot, rot=45, logy=True, grid=True)
+subplot.set_xlabel("Acquisition Year", fontdict=labels_font, labelpad=10)
+subplot.set_ylabel("Artworks Acquired", fontdict=labels_font)
+subplot.locator_params(nbins=40, axis='x')
+subplot.set_title("Tate Gallery Acquisitions", fontdict=title_font)
+fig.show()
+
+# Save to files
+fig.savefig('plot.png')
+fig.savefig('plot.svg', format='svg')
